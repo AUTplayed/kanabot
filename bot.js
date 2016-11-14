127a64f55759dd6b69f59b17c4a91c5cab3387d2
Philipp Hemmelmayr <Philipp Hemmelmayr>
2016-11-14 23:38:06 +0100

    added more debug function, removed cross-server functionality and changed how saving messages works

--- bot.js
+++ bot.js
@@ -2,18 +2,17 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var msglog = [];
 var errorlog = [];
-var count = 0;
 
 var http = require('http');
 http.createServer(function (req, res) { 
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("Errors:\n")
+    res.write("Log:\n")
     res.write(arrayToString(errorlog));
     res.end();
 }).listen(process.env.PORT || 5000);
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
+    errorlog.push(`Logged in as ${client.user.username}#${client.user.discriminator}`);
 });
 
 client.on('message', msg => {
@@ -26,8 +25,6 @@ client.on('message', msg => {
 });
 client.on('messageUpdate', (oldMessage, newMessage) => {
     msglog.push(oldMessage);
-    if (msglog.length >= 5)
-        msglog.shift();
 });
 client.login('MjQ3NjIwNzcyMzk3MzE4MTYz.CwsI0g.1QE29N_6Ts6n6p-NYGw0GiokFB0');
 
@@ -35,22 +32,19 @@ function reply(msg) {
     if (msg.isMentioned(client.user)) {
         var cleanmsg = clearMentions(msg.cleanContent);
         if (cleanmsg == 'gheat' || cleanmsg == 'gseng') {
-        	if(msglog.length==0){
+        	if(formatArray(msg.channel.name,msg.guild.name)==false){
         		msg.reply("nix zum seng");
         	}
-        	else{
-            	formatArray(msg.channel.name);
+        } else if (msg.author.id == 163651635845922816) {
+        	if(cleanmsg == 'debug'){
+            	msg.reply("Message Log\n" + msglog);
         	}
-        } else if (cleanmsg == 'debug' && msg.author.id == 163651635845922816) {
-            msg.reply("Message Log Length: " + msglog.length);
-            msg.reply("Messages since last edit: " + count);
-        }
-    } else {
-        if (msglog != [])
-            count++;
-        if (count >= 20) {
-            count = 0;
-            msglog = [];
+        	else if(cleanmsg == 'clearLog'){
+        		errorlog=[];
+        	}
+        	else if(cleanmsg == 'clearMessages'){
+        		msglog = [];
+        	}
         }
     }
 }
@@ -65,11 +59,15 @@ function clearMentions(msg) {
     return cleanmsg.trim();
 }
 
-function formatArray(channel) {
+function formatArray(channel,guild) {
     var toremove = [];
+    var replied = false;
     msglog.forEach(function(element) {
-    	if(element.channel.name==channel){
+    	if((Date.now()-element.createdTimestamp)>=3600000)
+    		toremove.push(msglog.indexOf(element));
+    	if(element.channel.name==channel&&element.guild.name==guild){
         	element.reply(element.cleanContent);
+        	replied = true;
         	toremove.push(msglog.indexOf(element));
     	}
     });
@@ -81,7 +79,8 @@ function formatArray(channel) {
     	else{
     		msglog = msglog.splice(element-(removed++),1);
     	}
-    })
+    });
+    return replied;
 }
 function arrayToString(array){
 	var s = "";
