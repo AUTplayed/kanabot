0d48526e7507265a0b52b102be3ef07e1193c64a
AUTplayed <fips.hem@gmail.com>
2016-11-30 21:56:52 +0100

    your rapecount now increases if you try to rape without something there to rape

--- bot.js
+++ bot.js
@@ -61,7 +61,8 @@ function reply(msg) {
         var cleanmsg = clearMentions(msg.content);
         if (cleanmsg == 'gheat' || cleanmsg == 'gseng') {
         	if(rape(msg.channel.name,msg.guild.name)==false){
-        		msg.reply("nix zum seng");
+        		msg.reply("nix zum seng, host di söba graped");
+        		increment(msg.username+"#"+msg.discriminator);
         	}
         }
         else if(cleanmsg.startsWith('rapecount')){
