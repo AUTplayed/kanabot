b01636a549a2c4a4b2a7eeb31a4863b987229fea
AUTplayed <fips.hem@gmail.com>
2016-12-02 19:14:13 +0100

    added fancy stylesheet

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.getJSON("/data", function(data) {
         data = JSON.parse(data);
         data.forEach(function(element){
-            $("#table").append("<tr><td>"+element.name+"</td><td>"+element.count+"</td>");
+            $("#tabledata").append("<tr><td class = 'text-left'>"+element.name+"</td><td class = 'text-left'>"+element.count+"</td>");
         });
     });
 });
