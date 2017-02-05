e71dc19d0c0768cde0974fde737df355615fd1fa
AUTplayed <fips.hem@gmail.com>
2017-02-05 14:05:51 +0100

    added pure link cases

--- yt.js
+++ yt.js
@@ -16,45 +16,68 @@ db.connectAndQuery("SELECT * FROM token WHERE name = 'yt'", function (rows) {
 module.exports.get = get;
 
 function get(q, followup, finished) {
-    getBody(baseurl + "search?part=snippet&q=" + q + "&type=video,playlist&key=" + key, function(body) {
-        if(item.length < 1){
-            finished(1,0);
+    if (q.startsWith("https://www.youtube.com/")) {
+        if (q.startsWith("https://www.youtube.com/watch?v=")) {
+            getVideo(filterId(q,"v"),followup,finished);
+        } else if (q.startsWith("https://www.youtube.com/playlist?list=")) {
+            getPlaylist(filterId(q,"list"),followup,finished);
+        }
+    }
+    getBody(baseurl + "search?part=snippet&q=" + q + "&type=video,playlist&key=" + key, function (body) {
+        console.log(body.items.length);
+        if (body.items.length < 1) {
+            finished(1, 0);
+            return;
         }
         var id = body.items[0].id;
         if (id.videoId) {
-            downloadInfo("https://www.youtube.com/watch?v=" + id.videoId, 0, function(info) {
-                followup(info);
-                if (finished)
-                    finished(1, 1);
-            });
-        } else {
-            var progress = 0;
-            var success = 0;
-            var length;
-            getAllPlaylistElements(id.playlistId, function(len) {
-                length = len
-            }, function(videoId) {
-                console.log(videoId);
-                
-                downloadInfo("https://www.youtube.com/watch?v="+videoId, 0, function(info) {
-                    progress++;
-                    followup(info);
-                    if (info.title) success++;
-                    if (progress == length) {
-                        if (finished)
-                            finished(length, success);
-                    }
-                });
-            });
+            getVideo(id.videoId, followup, finished);
+        } else if (id.playlistId) {
+            getPlaylist(id.playlistId, followup, finished);
         }
     });
 }
 
+function filterId(q,filter){
+    q = q.split("?"+v+"=")[1];
+    q = q.split("&")[0];
+    return q;
+}
+
+function getVideo(id, followup, finished) {
+    downloadInfo("https://www.youtube.com/watch?v=" + id, 0, function (info) {
+        followup(info);
+        if (finished)
+            finished(1, 1);
+    });
+}
+
+function getPlaylist(id, followup, finished) {
+    var progress = 0;
+    var success = 0;
+    var length;
+    getAllPlaylistElements(id, function (len) {
+        length = len
+    }, function (videoId) {
+        console.log(videoId);
+
+        downloadInfo("https://www.youtube.com/watch?v=" + videoId, 0, function (info) {
+            progress++;
+            followup(info);
+            if (info.title) success++;
+            if (progress == length) {
+                if (finished)
+                    finished(length, success);
+            }
+        });
+    });
+}
+
 function getAllPlaylistElements(id, len, followup) {
-    getBody(baseurl + "playlistItems?part=snippet&maxResults=50&playlistId=" + id + "&key=" + key, function(body) {
+    getBody(baseurl + "playlistItems?part=snippet&maxResults=50&playlistId=" + id + "&key=" + key, function (body) {
         var iterations = Math.ceil(body.pageInfo.totalResults / 50);
         len(body.pageInfo.totalResults);
-        body.items.forEach(function(item) {
+        body.items.forEach(function (item) {
             followup(item.snippet.resourceId.videoId);
         });
         if (iterations > 1) {
@@ -64,8 +87,8 @@ function getAllPlaylistElements(id, len, followup) {
 }
 
 function recursiveGetElements(id, pageToken, iterations, followup) {
-    getBody(baseurl + "playlistItems?part=snippet&maxResults=50&playlistId=" + id + "&pageToken=" + pageToken + "&key=" + key, function(body) {
-        body.items.forEach(function(item) {
+    getBody(baseurl + "playlistItems?part=snippet&maxResults=50&playlistId=" + id + "&pageToken=" + pageToken + "&key=" + key, function (body) {
+        body.items.forEach(function (item) {
             followup(item.snippet.resourceId.videoId);
         });
         if (--iterations > 1) {
@@ -75,7 +98,7 @@ function recursiveGetElements(id, pageToken, iterations, followup) {
 }
 
 function downloadInfo(url, failcount, followup) {
-    ytdl.getInfo(url, function(err, info) {
+    ytdl.getInfo(url, function (err, info) {
         if (!info || err) {
             console.log(err);
             failcount++;
@@ -90,12 +113,12 @@ function downloadInfo(url, failcount, followup) {
 }
 
 function getBody(url, followup) {
-    https.get(url, function(res) {
+    https.get(url, function (res) {
         var html = '';
-        res.on('data', function(data) {
+        res.on('data', function (data) {
             html += data;
         });
-        res.once('end', function() {
+        res.once('end', function () {
             followup(JSON.parse(html));
         })
     }).end();
